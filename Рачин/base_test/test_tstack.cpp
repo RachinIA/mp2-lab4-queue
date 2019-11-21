a1af6fb722ee35c0ee230b9eedf53b58facd40de
RachinIA <dikasz@mail.ru>
2019-11-21 21:16:26 +0300

    @RachinIA
    V/1.1

--- Рачин/base_test/test_tstack.cpp
+++ Рачин/base_test/test_tstack.cpp
@@ -13,25 +13,25 @@ TEST(TQueue, can_create_queue_with_negativ_length)
 {
 	ASSERT_ANY_THROW(TQueue<int>st(-1));
 }
-TEST(TQueue, check_IsEmpty_with_empty_stack)
+TEST(TQueue, check_IsEmpty_with_empty_queue)
 {
 	TQueue<int> st(5);
 	EXPECT_EQ(true, st.IsEmpty());
 }
-TEST(TQueue, check_IsEmpty_with_not_empty_stack)
+TEST(TQueue, check_IsEmpty_with_not_empty_queue)
 {
 	TQueue<int> st(5);
 	st.Put(2);
 	EXPECT_EQ(false, st.IsEmpty());
 }
-TEST(TQueue, check_IsFull_with_full_stack)
+TEST(TQueue, check_IsFull_with_full_queue)
 {
 	TQueue<int> st(2);
 	st.Put(1);
 	st.Put(2);
 	EXPECT_EQ(true, st.IsFull());
 }
-TEST(TQueue, check_IsFull_with_not_full_stack)
+TEST(TQueue, check_IsFull_with_not_full_queue)
 {
 	TQueue<int> st(5);
 	st.Put(2);
